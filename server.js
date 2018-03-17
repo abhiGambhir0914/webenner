2e62fe06c8446a8112fe1d77560a843b58172205
System Administrator <root@Neelams-MacBook-Pro.local>
2018-03-17 23:11:39 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(express.static('final'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+  res.sendFile('index.html');
+});
+
+app.listen(port , () => console.log(`Server set up at port: ${port}`));
